8758d50aab439550c621ad5933b8e2b2cb142263
iamAndreyGavrilov <79842149+iamAndreyGavrilov@users.noreply.github.com>
2022-04-26 20:59:30 +0500

    actions

--- src/components/xstateAddToCart.vue
+++ src/components/xstateAddToCart.vue
@@ -1,5 +1,8 @@
 <script setup>
-import { createMachine } from "xstate";
+import { assign, createMachine } from "xstate";
+import { useMachine } from "@xstate/vue";
+import axios from "axios";
+
 const props = defineProps({
   productId: Number,
   qty: {
@@ -8,65 +11,94 @@ const props = defineProps({
   },
 });
 
-const addToCartMachine = createMachine({
-  id: "addToCartMachine ",
-  context: {
-    productId: props.productId,
-    qty: props.qty,
-    errors: {},
-  },
+const addToCartMachine = createMachine(
+  {
+    id: "addToCartMachine ",
+    context: {
+      productId: props.productId,
+      qty: props.qty,
+      errors: {},
+    },
 
-  initial: "idle",
-  states: {
-    idle: {
-      on: {
-        UPDATE_QTY: {
-          actions: "assignQty",
-          cond: "minQty",
-          target: "idle",
-        },
-        ADD_TO_CART: {
-          target: "adding",
-        },
-      },
-    },
-    adding: {
-      entry: "clearErrors",
-      invoke: {
-        id: "addToCart",
-        src: "addToCart",
-        onDone: {
-          target: "recentyAdded",
-        },
-        onError: {
-          actions: "assignErrors",
-          target: "idle",
+    initial: "idle",
+    states: {
+      idle: {
+        on: {
+          UPDATE_QTY: {
+            actions: "assignQty",
+            cond: "minQty",
+            target: "idle",
+          },
+          ADD_TO_CART: {
+            target: "adding",
+          },
         },
       },
-    },
-    recentyAdded: {
-      after: {
-        1000: {
-          target: "idle",
+      adding: {
+        entry: "clearErrors",
+        invoke: {
+          id: "addToCart",
+          src: "addToCart",
+          onDone: {
+            target: "recentyAdded",
+          },
+          onError: {
+            actions: "assignErrors",
+            target: "idle",
+          },
         },
       },
-      on: {
-        UPDATE_QTY: {
-          actions: "assignQty",
-          cond: "minQty",
-          target: "recentyAdded",
+      recentyAdded: {
+        after: {
+          1000: {
+            target: "idle",
+          },
+        },
+        on: {
+          UPDATE_QTY: {
+            actions: "assignQty",
+            cond: "minQty",
+            target: "recentyAdded",
+          },
         },
       },
     },
   },
-});
+  {
+    actions: {
+      assignQty: assign({
+        qty: (ctx, event) => event.value,
+      }),
+      clearErrors: assign({
+        errors: {},
+      }),
+      assignErrors: assign({
+        errors: (ctx, event) => {
+          console.log(event);
+        },
+      }),
+    },
+    services: {
+      addToCart: (ctx) =>
+        axios.post("/api/cart-items", {
+          qty: ctx.qty,
+          productId: ctx.productId,
+        }),
+    },
+    guards: {
+      minQty: (ctx, event) => event.value >= 1,
+    },
+  }
+);
+const { state, send, service } = useMachine(addToCartMachine);
 </script>
 
 <template>
-  <form>
+  <form @submit.prevent="send('ADD_TO_CART')">
     <div class="flex space-x-4">
       <div class="flex items-center space-x-2">
         <button
+          @click="send({ type: 'UPDATE_QTY', value: state.context.qty - 1 })"
           class="p-1 rounded border-2 border-black hover:bg-yellow-400 focus:outline-none focus-visible:border-yellow-400"
           type="button"
         >
@@ -78,12 +110,17 @@ const addToCartMachine = createMachine({
         </button>
 
         <input
-          value="1"
+          @change="
+            (ev) =>
+              send({ type: 'UPDATE_QTY', value: parseInt(ev.target.value, 10) })
+          "
+          :value="state.context.qty"
           class="p-1 w-10 h-10 font-semibold text-center rounded border-2 border-black outline-none focus:ring-transparent focus:border-yellow-400"
           type="number"
         />
 
         <button
+          @click="send({ type: 'UPDATE_QTY', value: state.context.qty + 1 })"
           class="p-1 rounded border-2 border-black hover:bg-yellow-400 focus:outline-none focus-visible:border-yellow-400"
           type="button"
         >
@@ -94,7 +131,13 @@ const addToCartMachine = createMachine({
       <button
         class="inline-flex justify-center items-center px-6 py-2.5 text-sm font-semibold text-black bg-yellow-400 rounded-full border-2 border-transparent hover:bg-black hover:text-white focus:outline-none focus-visible:border-black"
       >
-        Add to cart
+        {{
+          state.matches("recentyAdded")
+            ? state.context.qty > 1
+              ? "Products were added"
+              : "Product was added"
+            : "Add to cart"
+        }}
       </button>
     </div>
 
