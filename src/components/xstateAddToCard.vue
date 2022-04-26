3f69936efbe8f356c042a753e8604c471c8d4026
iamAndreyGavrilov <79842149+iamAndreyGavrilov@users.noreply.github.com>
2022-04-26 18:50:03 +0500

    1

--- src/components/xstateAddToCard.vue
+++ src/components/xstateAddToCard.vue
@@ -1,10 +1,61 @@
 <script setup>
 import PlusIcon from "./PlusIcon";
+
 import MinusIcon from "./MinusIcon";
+
+const props = defineProps({
+  productId: Number,
+  qty: {
+    type: Number,
+    defauld: 1,
+  },
+});
 </script>
 
 <template>
-  <div>{{ PlusIcon }}</div>
+  <form>
+    <div class="flex space-x-4">
+      <div class="flex items-center space-x-2">
+        <button
+          class="p-1 rounded border-2 border-black hover:bg-yellow-400 focus:outline-none focus-visible:border-yellow-400"
+          type="button"
+        >
+          <MinusIcon class="h-5 w-5" />
+        </button>
+
+        <input
+          value="1"
+          class="p-1 w-10 h-10 font-semibold text-center rounded border-2 border-black outline-none focus:ring-transparent focus:border-yellow-400"
+          type="number"
+        />
+
+        <button
+          class="p-1 rounded border-2 border-black hover:bg-yellow-400 focus:outline-none focus-visible:border-yellow-400"
+          type="button"
+        >
+          <PlusIcon class="w-5 h-5" />
+        </button>
+      </div>
+
+      <button
+        class="inline-flex justify-center items-center px-6 py-2.5 text-sm font-semibold text-black bg-yellow-400 rounded-full border-2 border-transparent hover:bg-black hover:text-white focus:outline-none focus-visible:border-black"
+      >
+        Add to cart
+      </button>
+    </div>
+
+    <div class="mt-2 text-sm text-red-600">This is an error.</div>
+  </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+</style>
